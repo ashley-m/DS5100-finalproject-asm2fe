f18af93a2d712009f32185e7960f002e094e053c
ashley-m <asm2fe@virginia.edu>
2024-07-05 11:25:28 -0400

    next

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'monte carlo',
+    version = '1.0',
+    author = 'Ashley',
+    author_email = 'asm2fe@virginia.edu',
+    packages = ['montecarlo'],
+    url = '***',
+    license = 'LICENSE.txt',
+    description = '***',
+    long_description = open('README.md').read()
+)
